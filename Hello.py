589a02e1d1d5b6b9b9aae737e46547f6e921f39d
Klaus Weinberg <weinbergkeit@gmail.com>
2018-08-11 20:54:22 +0000

    first commit

---
+++ Hello.py
@@ -0,0 +1,91 @@
+
+
+#import sys
+# print(sys.path)
+
+
+#from matplotlib import pyplot as plt
+
+# plt.plot([1,2,3],[4,5,1])
+
+# plt.show()
+
+
+#import matplotlib.pyplot as plt
+#import matplotlib as mpl
+#import numpy as np
+
+#x = np.linspace(0, 20, 100)
+#plt.plot(x, np.sin(x))
+# plt.show()
+
+
+#print('http man hello')
+#print('this is fishing')
+
+#first_name = 'Florin'
+#last_name = 'Rentea'
+
+#sentence = f'My name is {first_name.upper()} {last_name.upper()}'
+# print(sentence)
+
+
+#calculation = f'4 time 11 is equal to {4 * 11}'
+# print(calculation)
+
+
+#from datetime import datetime
+
+#birthday = datetime(1990, 1, 1)
+
+
+#sentence = f'Remus has a birthday on {birthday:%B %d, %Y}'
+
+
+
+#dimensions = (200, 50)
+#print("Original dimensions")
+#for dimensions in dimensions:
+#    print(dimensions)
+
+#dimensions = (400, 100)   
+#print("\nModified dimensions:")
+#for dimension in dimensions:
+#    print(dimension)
+
+#def     describe_pet(animal_type, pet_name):
+#     """ Display information about a pet."""
+#     print("\nI have a " + animal_type + ".")
+#     print("my " + animal_type + "'s name is " + pet_name.title() + ".")
+
+#describe_pet('hamster', 'harry')
+
+
+#def greet_user(username):
+#    """Display a simple greeting."""
+#    print("Hello, " + username.title() + "!")
+#greet_user('Sarah')
+
+
+#def display_message(learn):
+#    print("Hello, I am learning about functions in " + learn.title() + "!")
+#display_message('python')    
+
+
+#class Car():
+#    """ A simple attempt to represent a car."""
+
+#    def __init__(self, make, model, year):
+#        """ Initialize attritutess to describe a car."""
+#        self.make = make
+##        self.model = model
+#        self.year = year
+    
+
+#    def get_descriptive_name(self):
+#        """Return a neatly formatted descriptive name."""
+#        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#        return long_name.title()
+
+##my_new_car = Car('audi', 'a4', 2016)
+#print(my_new_car.get_descriptive_name())
